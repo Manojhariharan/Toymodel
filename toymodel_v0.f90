d88643842278d93840e070dc3e918dcbda5f039e
Manoj Hari <153212275+Manojhariharan@users.noreply.github.com>
2025-04-25 18:01:17 +0100

    Update toymodel_v0.f90

--- toymodel_v0.f90
+++ toymodel_v0.f90
@@ -2,42 +2,44 @@
 ! Program: toymodel_v0
 ! Purpose: Minimal 1-layer, 1-pool SOM model with input and respiration
 !======================================================================!
-
 program toymodel_v0
     implicit none
 
     !------------------------------------------------------------------!
     ! Parameters
     !------------------------------------------------------------------!
-    integer, parameter :: nyears = 100               ! Simulation length (years)
-    real, parameter :: dt = 1.0                      ! Timestep (year)
-    real, parameter :: input_rate = 0.2              ! SOM input (kg C/m2/year)
-    real, parameter :: k_decay = 0.05                ! Decay rate (/year)
+    integer, parameter :: nyears = 100                 ! Simulation length (years)
+    integer, parameter :: nlayers = 1                  ! Number of layers
+    real, parameter :: dt = 1.0                        ! Timestep (year)
+    real, parameter :: input_rate = 0.2                ! SOM input (kg C/m2/year)
+    real, parameter :: k_decay = 0.05                  ! Decay rate (/year)
 
     !------------------------------------------------------------------!
     ! Variables
     !------------------------------------------------------------------!
-    real :: SOM                                      ! Soil organic matter (kg C/m2)
     real :: input, decay, respiration
-    integer :: t
+    real, dimension(nlayers) :: SOM                    ! SOM pool (kg C/m2)
+    integer :: t, i
 
     !------------------------------------------------------------------!
     ! Initialization
     !------------------------------------------------------------------!
-    SOM = 1.0                                         ! Initial SOM (kg C/m2)
+    SOM = 1.0                                          ! Initial SOM for each layer
     write(*,'(a)') 'Year    SOM_C     Input    Respired'
 
     !------------------------------------------------------------------!
     ! Time loop
     !------------------------------------------------------------------!
     do t = 1, nyears
-        input = input_rate
-        decay = k_decay * SOM
-        respiration = decay                          ! All decayed C is respired
+        do i = 1, nlayers
+            input = input_rate
+            decay = k_decay * SOM(i)
+            respiration = decay                        ! All decay becomes CO2
 
-        SOM = SOM + input - respiration
+            SOM(i) = SOM(i) + input - respiration
+        end do
 
-        write(*,'(i4,3f10.4)') t, SOM, input, respiration
+        write(*,'(i4,3f10.4)') t, SOM(1), input, respiration
     end do
 
 end program toymodel_v0
